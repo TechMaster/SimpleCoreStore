13b6f5d0ab3d9656a4160730611a8a767263cd60
minhcuong <cuong@techmaster.vn>
2016-09-10 18:24:30 +0700

    Create sample app

---
+++ RockCoreStore/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  RockCoreStore
+//
+//  Created by Techmaster on 9/10/16.
+//  Copyright © 2016 Techmaster Vietnam. All rights reserved.
+//
+
+import UIKit
+import CoreStore
+class ViewController: UIViewController {
+
+    let dataStack = DataStack(modelName: "HumanResource")
+    
+    func configureStorage() {
+        _ = dataStack.addStorage(
+            SQLiteStore(
+                fileName: "HumanResource.sql",
+               // configuration: "Default",
+                localStorageOptions: .RecreateStoreOnModelMismatch)
+            ,
+            completion: { (result) in
+                switch result {
+                case .Success(let storage):
+                    print("Successfully added sqlite store: \(storage)")
+                case .Failure(let error):
+                    print("Failed adding sqlite store with error: \(error)")
+                }
+        })
+        
+        CoreStore.defaultStack = dataStack
+        
+    }
+    
+    func addSampleData() {
+        dataStack.beginSynchronous { transaction in
+            
+            transaction.deleteAll(From(Person))
+            
+            let person1 = transaction.create(Into<Person>())
+            person1.name = "Cuong"
+            person1.gender = 1
+            
+            let person2 = transaction.create(Into<Person>())
+            person2.name = "Khue"
+            person2.gender = 0
+            
+            
+            transaction.commitAndWait()
+        }
+    }
+    
+    func getData() {
+        if let people = dataStack.fetchAll(From(Person)) {
+            for aPerson in people {
+                print("\(aPerson.name)")
+            }
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureStorage()
+        addSampleData()
+        getData()
+        
+    }
+
+
+
+}
+
